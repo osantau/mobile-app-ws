e0b5daea5db544d973e1f025600dac43403e7037
Octavian Santau <octavian.santau@palstor.ro>
2020-10-02 13:17:54 +0300

    First commit with create user.

---
+++ src/main/java/oct/soft/ws/service/UserService.java
@@ -0,0 +1,8 @@
+package oct.soft.ws.service;
+
+import oct.soft.ws.ui.shared.dto.UserDto;
+
+public interface UserService {
+
+	UserDto createUser(UserDto user);
+}
---
+++ src/main/java/oct/soft/ws/service/impl/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package oct.soft.ws.service.impl;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import oct.soft.ws.io.entity.UserEntity;
+import oct.soft.ws.repository.UserRepository;
+import oct.soft.ws.service.UserService;
+import oct.soft.ws.ui.shared.dto.UserDto;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public UserDto createUser(UserDto user) {
+		
+		UserEntity userEntity = new UserEntity();
+		BeanUtils.copyProperties(user, userEntity);		
+		userEntity.setEncryptedPassword("test");
+		userEntity.setUserId("testUserId");
+		UserEntity storedUserDetails = userRepository.save(userEntity);
+		UserDto returnValue = new UserDto();
+		BeanUtils.copyProperties(storedUserDetails, returnValue);
+		
+		return returnValue;
+	}
+
+}
